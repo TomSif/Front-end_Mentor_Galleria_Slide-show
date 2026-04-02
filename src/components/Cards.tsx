0178949dad4880c53bf4354cdef8f8e563c928b3
TomSif <thomas.sifferle.programmer@gmail.com>
2026-04-02 17:31:41 +0200

    feat(Cards): add component Cards accept props from Home, map is working , need to rework round-robin with shortest-column-first

---
+++ src/components/Cards.tsx
@@ -0,0 +1,22 @@
+import { Link } from "react-router";
+
+interface CardsProps {
+  name: string;
+  thumbnail: string;
+  artistName: string;
+  path: string;
+}
+
+function Cards({ name, thumbnail, artistName, path }: CardsProps) {
+  return (
+    <Link className="flex flex-col  relative w-full h-auto" to={path}>
+      <img className="w-full h-auto object-cover " src={thumbnail} alt={name} />
+      <div className="flex flex-col gap-2 items-start text-left absolute left-8 bottom-8">
+        <strong className="text-preset-2-mobile text-white">{name}</strong>
+        <span className="text-preset-5 text-white/75">{artistName}</span>
+      </div>
+    </Link>
+  );
+}
+
+export default Cards;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,36 @@
+import dataTyped from "../data";
+import Cards from "../components/Cards";
+import { getCorrectPath } from "../utils/generatePath";
+
+const COLS = 4;
+
 function Home() {
+  // Distribuer les items en round-robin dans 4 colonnes
+  const columns = Array.from({ length: COLS }, () => [] as typeof dataTyped);
+
+  dataTyped.forEach((card, i) => {
+    columns[i % COLS].push(card);
+  });
+
   return (
-    <>
-      <h1 className="bg-black text-white">HOME</h1>
-    </>
+    <main className="w-full max-w-[1360px] px-6 flex flex-col items-center">
+      <div className="flex gap-10 w-full">
+        {columns.map((col, colIndex) => (
+          <ul key={colIndex} className="flex flex-col gap-10 flex-1 ">
+            {col.map((card) => (
+              <li key={card.name} className="w-full max-h-325">
+                <Cards
+                  name={card.name}
+                  thumbnail={card.images.thumbnail}
+                  artistName={card.artist.name}
+                  path={getCorrectPath(card.name)}
+                />
+              </li>
+            ))}
+          </ul>
+        ))}
+      </div>
+    </main>
   );
 }
 
